0ebade71b3e6e36f3c17db17d33457a0ae64cff1
Alexander Russell <awjrussell@live.co.uk>
2022-05-27 10:19:59 +0100

    AB4692 <Ali> WIP - Implement SearchMenuQueryHandler for DynamoDB (#165)

--- src/api/xxAMIDOxx.xxSTACKSxx.Application.QueryHandlers/SearchMenuQueryHandler.cs
+++ src/api/xxAMIDOxx.xxSTACKSxx.Application.QueryHandlers/SearchMenuQueryHandler.cs
@@ -3,11 +3,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Amido.Stacks.Application.CQRS.Queries;
 using Amido.Stacks.Data.Documents.Abstractions;
+#if (DynamoDb)
+using Amazon.DynamoDBv2.DocumentModel;
+using Amido.Stacks.DynamoDB.Abstractions;
+#endif
 using xxAMIDOxx.xxSTACKSxx.CQRS.Queries.SearchMenu;
 using xxAMIDOxx.xxSTACKSxx.Domain;
 
 namespace xxAMIDOxx.xxSTACKSxx.Application.QueryHandlers;
 
+#if (DynamoDb)
+public class SearchMenuQueryHandler : IQueryHandler<SearchMenu, SearchMenuResult>
+{
+    private readonly IDynamoDbObjectSearch<Menu> search;
+
+    public SearchMenuQueryHandler(IDynamoDbObjectSearch<Menu> search)
+    {
+        this.search = search;
+    }
+
+    public Task<SearchMenuResult> ExecuteAsync(SearchMenu criteria)
+    {
+        if (criteria == null)
+            throw new ArgumentException("A valid SearchMenuQueryCriteria os required!");
+
+        // https://rules.sonarsource.com/csharp/RSPEC-4457
+        return ExecuteInternalAsync(criteria);
+    }
+
+    private async Task<SearchMenuResult> ExecuteInternalAsync(SearchMenu criteria)
+    {
+        var searchTerm = string.Empty;
+
+        if (!string.IsNullOrEmpty(criteria.SearchText))
+            searchTerm = criteria.SearchText.Trim();
+
+        var scanFilter = new ScanFilter();
+        scanFilter.AddCondition("Name", ScanOperator.Contains, searchTerm);
+
+        var results = await search.ScanAsync(scanFilter);
+
+        var result = new SearchMenuResult();
+
+        if (!results.IsSuccessful)
+            return result;
+
+        result.Results = results.Content.Select(SearchMenuResultItem.FromDomain);
+
+        return result;
+    }
+}
+#else
 public class SearchMenuQueryHandler : IQueryHandler<SearchMenu, SearchMenuResult>
 {
     readonly IDocumentSearch<Menu> storage;
@@ -47,7 +93,7 @@ public class SearchMenuQueryHandler : IQueryHandler<SearchMenu, SearchMenuResult
         var results = await storage.Search(
             itemFilter =>
                 (string.IsNullOrEmpty(searchTerm) || itemFilter.Name.Contains(searchTerm)) &&
-                //Nullable types must have a value when passed to a seach, this is why we convert it to non nullable and pass a boolean check
+                //Nullable types must have a value when passed to a search, this is why we convert it to non nullable and pass a boolean check
                 (!restaurantIdProvided || itemFilter.TenantId == tenantId),
             null,
             pageSize,
@@ -65,3 +111,4 @@ public class SearchMenuQueryHandler : IQueryHandler<SearchMenu, SearchMenuResult
         return result;
     }
 }
+#endif
